1cdcbe0c75b5b9e86d2d3417cde0280849642a86
Frolenko Dima <mail@frolenkodima.ru>
2023-09-13 19:07:42 +0300

    fix: server Подключить забытый хендлер для редактирования

--- cloud/main.go
+++ cloud/main.go
@@ -17,8 +17,7 @@ func init() {
 	Router.Handle("/*", fileServer)
 	// admining
 	Router.Route("/edit", func(r chi.Router) {
-		r.Use(handlers.Authorization)
-		r.Get("/*", http.NotFound)
+		r.Get("/*", handlers.Edit)
 	})
 
 	Router.Get("/login", http.NotFound)
