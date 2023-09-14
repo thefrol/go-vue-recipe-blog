282e92e32fb57cc367d872c4d1dbbf121b2aa494
Frolenko Dima <mail@frolenkodima.ru>
2023-09-14 04:14:23 +0300

    fix: internal/handlers убрать лишнюю проверку и лишнюю переменную

--- internal/handlers/edit.go
+++ internal/handlers/edit.go
@@ -1,22 +1,9 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
-// TODO
-// Далее хранить где-то токен	....
-const authContextParam = "auth"
-
 func Edit(w http.ResponseWriter, r *http.Request) {
-	ii := r.Context().Value(authContextParam)
-	var auth string
-	var ok bool
-	if auth, ok = ii.(string); !ok || auth != "ok" {
-		fmt.Println("in edit handler not found auth, big error in logic")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 	w.Write([]byte("Вы авторизированы. Привет! Могу сказать сколько ещё будет жить токен"))
 }
